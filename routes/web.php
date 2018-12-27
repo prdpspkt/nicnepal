a7bfe7e31c1d799ae55e7847d93898121cbaca4b
Your Name <you@example.com>
2018-12-27 15:05:50 +0545

    lo

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ Route::post("/notice_types/edit/{id}", "NoticeTypeController@update");
 Route::get("/notice_types/delete/{id}", "NoticeTypeController@delete");
 
 //API Section
-Route::get("/api/v1", "ApiController@index");
+Route::get("/api/notices", "ApiController@index");
 Route::get("/api/v1/offices/{id}", "ApiController@offices");
 Route::get("/api/v1/otypes/{id}", "ApiController@office_types");
 Route::get("/api/v1/ntypes/{id}", "ApiController@notice_types");
